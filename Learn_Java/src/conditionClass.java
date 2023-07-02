0330a7f40e07ef23c9b88df192377fedccf3dd22
fouuuadi <fouadlamnaouar18@gmail.com>
2023-07-02 21:14:07 +0200

    suite condition

--- Learn_Java/src/conditionClass.java
+++ Learn_Java/src/conditionClass.java
@@ -1,11 +1,41 @@
 public class conditionClass {
-    public static void main(String[] args) {
-        int monAge = 16;
-        boolean estMajeur = monAge >= 18;
-        if(estMajeur){
-            System.out.println("je suis majeur");
-        } else {
-           System.out.println("je suis mineur"); 
+    // public static void main(String[] args) {
+    //     int monAge = 16;
+    //     boolean estMajeur = monAge >= 18;
+    //     if(estMajeur){
+    //         System.out.println("je suis majeur");
+    //     } else {
+    //        System.out.println("je suis mineur"); 
+    //     }
+    // }
+    // public static void main(String[] args){
+    //     int argentDePoche = 15000;
+    //     int porche = 20000;
+    //     int mercedes = 10000;
+    //     int twingo = 2000;
+        
+    //     if(argentDePoche >= porche){
+    //         System.out.println("Tu peux acheter cette porche !");
+    //     } else if(argentDePoche >= mercedes){
+    //         System.out.println("Tu peux acheter cette mercedes !");
+    //     }else if(argentDePoche >= twingo){
+    //         System.out.println("Tu peux acheter cette twingo !");
+    //     }
+    // }
+    public static void main(String[] args){
+        int argentDePoche = 15000;
+        int porche = 20000;
+        int mercedes = 10000;
+        int twingo = 2000;
+        
+        if(argentDePoche >= porche){
+            System.out.println("Tu peux acheter cette porche !");
         }
-    }
+        if(argentDePoche >= mercedes){
+            System.out.println("Tu peux acheter cette mercedes !");
+        }
+        if(argentDePoche >= twingo){
+            System.out.println("Tu peux acheter cette twingo !");
+        }
+    }    
 }
