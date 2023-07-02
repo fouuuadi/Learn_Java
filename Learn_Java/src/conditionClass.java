b7e85642ee462defdf0e71a49a3672398bede907
fouuuadi <fouadlamnaouar18@gmail.com>
2023-07-02 22:06:25 +0200

    switch case

--- Learn_Java/src/conditionClass.java
+++ Learn_Java/src/conditionClass.java
@@ -22,20 +22,48 @@ public class conditionClass {
     //         System.out.println("Tu peux acheter cette twingo !");
     //     }
     // }
-    public static void main(String[] args){
-        int argentDePoche = 15000;
-        int porche = 20000;
-        int mercedes = 10000;
-        int twingo = 2000;
+    // public static void main(String[] args){
+    //     int argentDePoche = 15000;
+    //     int porche = 20000;
+    //     int mercedes = 10000;
+    //     int twingo = 2000;
         
-        if(argentDePoche >= porche){
-            System.out.println("Tu peux acheter cette porche !");
-        }
-        if(argentDePoche >= mercedes){
-            System.out.println("Tu peux acheter cette mercedes !");
-        }
-        if(argentDePoche >= twingo){
-            System.out.println("Tu peux acheter cette twingo !");
-        }
-    }    
+    //     if(argentDePoche >= porche){
+    //         System.out.println("Tu peux acheter cette porche !");
+    //     }
+    //     if(argentDePoche >= mercedes){
+    //         System.out.println("Tu peux acheter cette mercedes !");
+    //     }
+    //     if(argentDePoche >= twingo){
+    //         System.out.println("Tu peux acheter cette twingo !");
+    //     }
+    // }
+        // public static void main(String[] args){
+        //         int monAge = 25;
+                
+        //         if(monAge < 18){
+        //             System.out.println("Je vais a l'école !");
+        //         }
+        //         else if(monAge >= 18 && monAge <= 65){
+        //             System.out.println("Je travaille !");
+        //         }
+        //         else{
+        //             System.out.println("Je suis a la retraite !");
+        //         }
+        //     }
+        public static void main(String[] args){
+                int positionMarathon = 3;
+                
+                switch(positionMarathon){
+                    case 1:
+                    System.out.println("Médaille d'or");
+                    break;
+                    case 2:
+                    System.out.println("Médaille d'argent");
+                    break;
+                    case 3:
+                    System.out.println("Médaille de bronze");
+                    break;
+                }
+            }
 }
