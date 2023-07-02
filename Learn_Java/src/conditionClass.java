4da06016ccf189d1e928d9c8264b91abf04c8ffc
fouuuadi <fouadlamnaouar18@gmail.com>
2023-07-02 20:47:31 +0200

    ajout condition

---
+++ Learn_Java/src/conditionClass.java
@@ -0,0 +1,11 @@
+public class conditionClass {
+    public static void main(String[] args) {
+        int monAge = 16;
+        boolean estMajeur = monAge >= 18;
+        if(estMajeur){
+            System.out.println("je suis majeur");
+        } else {
+           System.out.println("je suis mineur"); 
+        }
+    }
+}
